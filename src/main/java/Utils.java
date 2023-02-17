02c485e830fd8a4ea72161fdd21b0e9496c1e207
Eive1Me <evangelzm@gmail.com>
2023-02-17 12:27:36 +0300

    working program

---
+++ src/main/java/Utils.java
@@ -0,0 +1,6 @@
+public abstract class Utils {
+    public static double compressDouble(double value) {
+        int valueInt = (int) Math.round(value * 100);
+        return ((double) valueInt) / 100;
+    }
+}
---
+++ src/main/java/mathelement/Operator.java
@@ -0,0 +1,6 @@
+package mathelement;
+
+public enum Operator {
+    LESS_OR_EQUAL,
+    GREATER_OR_EQUAL
+}
